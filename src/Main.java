009aa7f421d9f8f6e3914612056cb3e1d597d3bc
Greycell <dominik.severin@yandex.com>
2023-02-06 11:18:28 +0100

    comm

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         File file = new File("mask.jpg");
         File key = new File("public.key");
-        //Encryption.encrypt(file);
+        Encryption.encrypt(file);
         KeyPair kp = new KeyPair(Files.readAllBytes(key.toPath()), Files.readAllBytes(file.toPath()));
-        Encryption.decrypt(file, kp);
+        //Encryption.decrypt(file, kp);
     }
 }
